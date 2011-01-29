3365299b89574a16d02b0f751055aa71c8749688
Roberto Guido <bob4mail@gmail.com>
2011-01-29 22:50:46 +0000

    bug #190: errore nel download dei files caricati in 'uploads/'

--- src/org/barberaware/public/server/downloader.php
+++ src/org/barberaware/public/server/downloader.php
@@ -17,17 +17,24 @@
  *  along with this program.  If not, see <http://www.gnu.org/licenses/>.
  */
 
+require_once ( "utils.php" );
+
 $path = $_GET [ 'path' ];
+if ( isset ( $path ) == false )
+	error_exit ( "Richiesta non specificata" );
+
+if ( check_session () == false )
+	error_exit ( "Sessione non autenticata" );
 
 /*
 	Solo i contenuti della cartella "uploads" possono essere scaricati con questo metodo
 */
 $tokens = explode ( '/', $path );
-if ( $tokens [ 0 ] != 'uploads' )
-	exit ( 0 );
+if ( $tokens [ 1 ] != 'uploads' )
+	error_exit ( "Path non valido" );
 
 $filesystem = '../' . $path;
-$filename = $tokens [ 1 ];
+$filename = $tokens [ 2 ];
 
 header ( "Content-Type: application/octet-stream" );
 header ( "Content-Disposition: disposition-type=attachment; filename=\"$filename\"" );
